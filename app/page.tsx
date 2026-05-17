7f187467b86e65359d249fcd50215a8b04d9381d
Jacobcharendoff <jacobcharendoff@gmail.com>
2026-05-16 20:50:18 -0400

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,36 @@
-import { Hero, TheWhy, Photography, Problems, Flywheel, TAM, Ask } from '@/components/Sections';
+import { Hero, TheWhy, Photography, Problems, Flywheel, TAM } from '@/components/Sections';
+import {
+  SideNav,
+  VerticalMarquee,
+  TimCase,
+  HowItWorks,
+  Pricing,
+  Moat,
+  Competition,
+  Pipeline,
+  Team,
+  AskV2,
+} from '@/components/ExtendedSections';
 
 export default function InvestorPage() {
   return (
-    <main>
+    <main id="top" className="relative">
+      <SideNav />
       <Hero />
+      <VerticalMarquee />
       <TheWhy />
       <Photography />
+      <TimCase />
       <Problems />
       <Flywheel />
+      <HowItWorks />
+      <Pricing />
+      <Moat />
+      <Competition />
       <TAM />
-      <Ask />
+      <Pipeline />
+      <Team />
+      <AskV2 />
     </main>
   );
 }
